6a7ee8cb6261aecc62590f83bc4ef32137f94992
buda1994 <hector.torres@unosquare.com>
2017-09-27 09:33:51 -0500

    Exercises 1-2

---
+++ Exercises,1-2/scripts.js
@@ -0,0 +1,80 @@
+//Use this file to implement Part One of your project
+
+
+
+
+
+var animal = {};
+
+animal.username = 'far'
+console.log(animal.username)
+
+animal['tagline'] = 'rat'
+console.log(animal.tagline)
+
+var noises = []
+
+animal['noises'] = noises
+
+console.log(animal)
+
+
+var count
+for (var arc in animal){
+  count++
+  if (arc == 'username'){
+    console.log('Hi my name is ' + animal[arc]); 
+  }
+  else if (arc == 'tagline'){
+    console.log('I like to say ' + animal[arc]);
+  }
+  
+}
+
+
+/////////////////////////////////////////////////////////////////
+
+var noiseArray = ['arc'];
+
+noiseArray.unshift('tar');
+noiseArray.push('mar');
+
+noiseArray[3]='zar';
+
+console.log(noiseArray.length)
+
+console.log(noiseArray);
+
+
+animal.noises=noiseArray
+
+console.log(animal);
+
+////////////////////////////////////////////////////////////////
+
+var animals = [];
+
+animals.push(animal);
+
+
+var quackers = { username: 'DaffyDuck', tagline: 'Yippeee!', noises: ['quack', 'honk', 'sneeze', 'growl'] } 
+animals.push(quackers);
+console.log(animals);
+
+
+var cat= { username: 'Dagreguck', tagline: 'Yipgrge!', noises: ['qujhgj', 'trek', 'sn543ze', 'gryutl'] } 
+animals.push(cat);
+
+var dog= { username: 'ewtryck', tagline: 'mnbmge!', noises: ['quj65hj5', 'startrek', 'smnvbc', 'grewtlrew'] } 
+animals.push(dog);
+
+console.log(animals.length)
+
+
+
+////////////////////////////////////////////////////////////////
+
+
+
+
+
